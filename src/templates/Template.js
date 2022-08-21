9d22955a871d0a758939530bbfb5cb7fcbf09206
Carlos-Leon98 <carlosaleonrivas@gmail.com>
2022-08-20 20:20:31 -0500

    Fixing bug

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -26,14 +26,14 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="${twitter}" />
-          </a>
-          <a href="https://github.com/gndx">
-            <img src="${github}" />
-          </a>
-          <a href="https://instagram.com/gndx">
-            <img src="${instagram}" />
+        <a href="https://twitter.com/gndx">
+        <img src="${twitter}" />
+      </a>
+      <a href="https://github.com/gndx">
+        <img src="${github}" />
+      </a>
+      <a href="https://instagram.com/gndx">
+        <img src="${instagram}" />
           </a>
         </div>
       </div>
